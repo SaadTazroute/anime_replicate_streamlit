c7f6522fe9abd637c4408f305a19a26d102b301a
SaadTazroute <Saad.tazroute@gmail.com>
2023-02-24 02:17:09 +0100

    create button

--- main.py
+++ main.py
@@ -1,51 +1,79 @@
+import gc
+
 from PIL import Image
 
 import utils
-from utils import inputs, layout_streamlit
+from utils import inputs
 import streamlit as st
-import replicate
 import os
 import requests # request img from web
 import shutil # save img locally
 from utils import load_gan
-
+import logging
 os.environ["REPLICATE_API_TOKEN"] = "81f2e4dc54d050256a9aa63fc1ad0679aa35a287"
 
 
 def main() :
+    logging.basicConfig(format="\n%(asctime)s\n%(message)s", level=logging.INFO, force=True)
+
     model,version = load_gan()
+    ##########################"
+    st.set_page_config(page_title="Anime_Gan", page_icon="🤖")
+    st.write(
+        """<style>
+        [data-testid="column"] {
+            width: calc(50% - 1rem);
+            flex: 1 1 calc(50% - 1rem);
+            min-width: calc(50% - 1rem);
+        }
+        </style>""",
+        unsafe_allow_html=True,
+    )
+    #############################
+    st.title("Anime Generation")
 
-
-    layout_streamlit()
-    prompt = st.text_input('Enter Text')
-
-
-    # https://replicate.com/cjwbw/anything-v3-better-vae/versions/09a5805203f4c12da649ec1923bb7729517ca25fcac790e640eaa9ed66573b65#input
     inputs = utils.inputs
-    inputs['prompt'] = prompt
-    # print(inputs["prompt"])
-    # print(inputs)
-    # https://replicate.com/cjwbw/anything-v3-better-vae/versions/09a5805203f4c12da649ec1923bb7729517ca25fcac790e640eaa9ed66573b65#output-schema
+    print(inputs)
+    inputs['prompt'] = st.text_input('Enter Text',placeholder="write a description")
 
-    # Add a generate button
-    if st.button("Generate"):
+    col1, col2 = st.columns(2)
+    image_spin_holder = st.empty()
 
-        output = version.predict(**inputs)
+    #
+    # with image_spin_holder:
+    #     with st.spinner("Please wait while your Tweet is being generated..."):
+    with col1:
+        st.session_state.prompt =  st.button(
+            label="Generate text",
+            type="primary",
+            on_click=utils.generate_text,
+            args=(version, inputs, image_spin_holder)
+        )
+    with col2:
+        st.session_state.feeling_lucky = st.button(
+            label="Feeling lucky",
+            type="secondary",
+            on_click=utils.generate_text_lucky,
+            args=(version, inputs,image_spin_holder),
+        )
+    gc.collect()
+#
+    # if st.button("Generate"):
+    #
+    #     output = version.predict(**inputs)
+    #
+    #     ## image saving :
+    #     res = requests.get(output[0])
+    #     img_data = res.content
+    #
+    #     st.image(output, caption='Image from link', use_column_width=True)
+    #     # st.image(img_data, caption='Image from link', use_column_width=True)
+    #
+    # if st.button('Download'):
+    #     # st.markdown("<a href=" + output[0] + " download>Download Image</a>", unsafe_allow_html=True)
+    #     st.markdown('<a href="' + model.to_base64(output) + '" download>Download Image</a>', unsafe_allow_html=True)
+        # st.markdown("<a href=" + image_url + " download>Download Image</a>", unsafe_allow_html=True)
 
-
-        ## image saving :
-        res = requests.get(output[0])
-        img_data = res.content
-        # import io
-        # image = Image.open(io.BytesIO(img_data))
-
-        st.image(output, caption='Image from link', use_column_width=True)
-
-        if st.button('Download'):
-            # st.markdown("<a href=" + output[0] + " download>Download Image</a>", unsafe_allow_html=True)
-            st.markdown('<a href="' + model.to_base64(output) + '" download>Download Image</a>', unsafe_allow_html=True)
-            # st.markdown("<a href=" + image_url + " download>Download Image</a>", unsafe_allow_html=True)
-        #
         # # Add a share button
         # if st.button("Share on Twitter"):
         #     st.text("Share this on Twitter")
@@ -55,7 +83,6 @@ def main() :
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     main()
 
 
--- utils.py
+++ utils.py
@@ -1,63 +1,20 @@
 import replicate
-
+import logging
 import variables
 import random
 import streamlit as st
+import requests
 
 list_of_prompts = variables.list_of_prompts
 
-print(list_of_prompts)
+# print(list_of_prompts)
 # prompt = "lol"
-width  = 512
-height = 512
-num_outputs = 1
 
-prompt = None
-if prompt == None :
-    prompt = random.choice(list_of_prompts)
 # if len(prompt.split(" ")) > 40 :
 #     print("Prompt too long it should be under 40 char")
 #     prompt = prompt[:40]
 
-inputs = {
-    # Input prompt
-    'prompt': prompt,
-    # The prompt or prompts not to guide the image generation (what you do
-    # not want to see in the generation). Ignored when not using guidance.
-    # 'negative_prompt': ...,
 
-    # Width of output image. Maximum size is 1024x768 or 768x1024 because
-    # of memory limits
-    'width': width,
-
-    # Height of output image. Maximum size is 1024x768 or 768x1024 because
-    # of memory limits
-    'height':height,
-
-    # Number of images to output
-    'num_outputs': num_outputs,
-
-    # Number of denoising steps
-    # Range: 1 to 500
-    'num_inference_steps': 20,
-
-    # Scale for classifier-free guidance
-    # Range: 1 to 20
-    'guidance_scale': 7,
-
-    # Choose a scheduler.
-    'scheduler': "DPMSolverMultistep",
-
-
-}
-
-def layout_streamlit():
-    st.set_page_config(page_title="My App")
-    st.title("Anime Generation")
-    # st.header("this is the markdown")
-    # st.markdown("this is the header")
-    # st.subheader("this is the subheader")
-    # st.caption("this is the caption")
 
 def load_gan():
     model = replicate.models.get("cjwbw/anything-v3-better-vae")
@@ -65,4 +22,76 @@ def load_gan():
     version = model.versions.get("09a5805203f4c12da649ec1923bb7729517ca25fcac790e640eaa9ed66573b65")
     return model,version
 
+logging.basicConfig(format="\n%(asctime)s\n%(message)s", level=logging.INFO, force=True)
+
+# def generate_images(prompt:str):
+#     model,version = load_gan()
+#
+#     if st.session_state.n_requests >= 5:
+#         st.session_state.text_error = "Too many requests. Please wait a few seconds before generating another text or image."
+#         logging.info(f"Session request limit reached: {st.session_state.n_requests}")
+#         st.session_state.n_requests = 1
+#         return
+#     with image_spinner_placeholder:
+#         with st.spinner("Please wait while your image is being generated..."):
+#
+#
+#             st.session_state.n_requests += 1
+#             st.session_state.image = openai.image(processed_prompt)
+#             logging.info(f"Tweet: {prompt}\nImage prompt: {processed_prompt}")
+
+
+def generate_text(version,inputs,placeholder):
+
+
+    with st.spinner("Please wait while your image is being generated..."):
+        # if len(inputs["prompt"])<2:
+        #     st.session_state.text_error = "Please enter a correct description ! "
+        # st.session_state.text_error = False
+        # if st.session_state.text_error:
+        #     print("mok")
+        #     st.error(st.session_state.text_error)
+        col1, col2,col3,col4 = st.columns(4)
+        print(inputs)
+        output = version.predict(**inputs)
+        print(output)
+        with col1 :
+            ## image saving :
+            res = requests.get(output[0])
+            img_data = res.content
+
+            st.image(output, caption='Generated image', use_column_width='auto')
+
+
+
+def generate_text_lucky(version,inputs,placeholder):
+    with st.spinner("Please wait while your image is being generated..."):
+
+        inputs["prompt"] =  random.choice(list_of_prompts)
+
+        print(inputs)
+        output = version.predict(**inputs)
+
+        ## image saving :
+        res = requests.get(output[0])
+        img_data = res.content
+        col1, col2,col3,col4 = st.columns(4)
+        with col1 :
+            st.image(output, caption='Image from link', use_column_width="auto")
+
+
+
+height = 768
+width  = 768
+num_outputs = 1
+inputs = {
+    # Input prompt
+    'prompt': random.choice(list_of_prompts),
+    'width': width,
+    'height':height,
+    'num_outputs': num_outputs,
+    'num_inference_steps': 20,
+    'guidance_scale': 7,
+    'scheduler': "DPMSolverMultistep",
+    }
 
